c9617c4e9c066a2c41f58c1ae3730d6efe1a0106
watanta <watanabe101watanabe@yahoo.co.jp>
2021-10-10 14:08:28 +0000

    add two model training

--- test.py
+++ test.py
@@ -26,16 +26,9 @@ def seed_everything(seed_value):
         torch.backends.cudnn.deterministic = True
         torch.backends.cudnn.benchmark = True
 
-
-def to_label(action):
+def to_label_for_citytile(action):
     strs = action.split(' ')
-    if strs[0] == "m" or strs[0] == "bcity": # if worker or cart
-        unit_id = strs[1]
-        if strs[0] == 'm':
-            label = {'c': None, 'n': 0, 's': 1, 'w': 2, 'e': 3}[strs[2]]
-        elif strs[0] == 'bcity':
-            label = 4
-    elif strs[0] == "bw" or strs[0] == "bc" or strs[0] == "r": # if citytile
+    if strs[0] == "bw" or strs[0] == "bc" or strs[0] == "r": # if citytile
         unit_id = strs[1]+" "+strs[2] # citytileはidを持たないので、位置を入れて自分を判別
         if strs[0] == "r":
             label = 0
@@ -48,6 +41,16 @@ def to_label(action):
         label = None
     return unit_id, label
 
+def to_label_for_worker(action):
+    strs = action.split(' ')
+    unit_id = strs[1]
+    if strs[0] == 'm':
+        label = {'c': None, 'n': 0, 's': 1, 'w': 2, 'e': 3}[strs[2]]
+    elif strs[0] == 'bcity':
+        label = 4
+    else:
+        label = None
+    return unit_id, label
 
 
 def depleted_resources(obs):
@@ -56,11 +59,23 @@ def depleted_resources(obs):
             return False
     return True
 
+def action_is_worker(action):
+    if "u" in action: #TODO ほんとにこれでいいか
+        return True
+    else:
+        return False
+
+def action_is_citytile(action):
+    if ("r" in action) or ("bw" in action) or ("bc" in action):
+        return True
+    else:
+        return False
+
 
 def create_dataset_from_json(episode_dir, team_name='Toad Brigade'): 
     obses = {}
-    samples = []
-    append = samples.append
+    woker_samples = []
+    citytile_samples = []
     
     episodes = [path for path in Path(episode_dir).glob('*.json') if 'output' not in path.name]
     for filepath in tqdm(episodes): 
@@ -89,20 +104,27 @@ def create_dataset_from_json(episode_dir, team_name='Toad Brigade'):
                 obses[obs_id] = obs
                                 
                 for action in actions:
-                    unit_id, label = to_label(action)
-                    if label is not None: # Units
-                        append((obs_id, unit_id, label))
+                    if action_is_worker(action):
+                        unit_id, label = to_label_for_worker(action)
+                        if label is not None: #transfer落とす
+                            woker_samples.append((obs_id, unit_id, label))
+                    elif action_is_citytile(action): 
+                         unit_id, label = to_label_for_citytile(action)
+                         citytile_samples.append((obs_id, unit_id, label))
+                    else: #cart
+                        unit_id = None
+                        label = None
 
-    return obses, samples
+    return obses, woker_samples, citytile_samples
 
 # Input for Neural Network
-def make_input(obs, unit_id):
+def make_input_worker(obs, unit_id):
     width, height = obs['width'], obs['height']
     x_shift = (32 - width) // 2
     y_shift = (32 - height) // 2
     cities = {}
     
-    b = np.zeros((22, 32, 32), dtype=np.float32)
+    b = np.zeros((20, 32, 32), dtype=np.float32)
     
     for update in obs['updates']:
         strs = update.split(' ')
@@ -132,12 +154,78 @@ def make_input(obs, unit_id):
                 )
         elif input_identifier == 'ct':
             # CityTiles
+            team = int(strs[1])
+            city_id = strs[2]
+            x = int(strs[3]) + x_shift
+            y = int(strs[4]) + y_shift
+            idx = 8 + (team - obs['player']) % 2 * 2
+            b[idx:idx + 2, x, y] = (
+                1,
+                cities[city_id]
+            )
+        elif input_identifier == 'r':
+            # Resources
+            r_type = strs[1]
+            x = int(strs[2]) + x_shift
+            y = int(strs[3]) + y_shift
+            amt = int(float(strs[4]))
+            b[{'wood': 12, 'coal': 13, 'uranium': 14}[r_type], x, y] = amt / 800
+        elif input_identifier == 'rp':
+            # Research Points
+            team = int(strs[1])
+            rp = int(strs[2])
+            b[15 + (team - obs['player']) % 2, :] = min(rp, 200) / 200
+        elif input_identifier == 'c':
+            # Cities
+            city_id = strs[2]
+            fuel = float(strs[3])
+            lightupkeep = float(strs[4])
+            cities[city_id] = min(fuel / lightupkeep, 10) / 10
+    
+    # Day/Night Cycle
+    b[17, :] = obs['step'] % 40 / 40
+    # Turns
+    b[18, :] = obs['step'] / 360
+    # Map Size
+    b[19, x_shift:32 - x_shift, y_shift:32 - y_shift] = 1
+
+    return b
+
+def make_input_citytile(obs, unit_id):
+    width, height = obs['width'], obs['height']
+    x_shift = (32 - width) // 2
+    y_shift = (32 - height) // 2
+    cities = {}
+    
+    b = np.zeros((20, 32, 32), dtype=np.float32)
+    
+    for update in obs['updates']:
+        strs = update.split(' ')
+        input_identifier = strs[0]
+        
+        if input_identifier == 'u':
+            x = int(strs[4]) + x_shift
+            y = int(strs[5]) + y_shift
+            wood = int(strs[7])
+            coal = int(strs[8])
+            uranium = int(strs[9])
+            # Units
+            team = int(strs[2])
+            cooldown = float(strs[6])
+            idx = 2 + (team - obs['player']) % 2 * 3
+            b[idx:idx + 3, x, y] = (
+                1,
+                cooldown / 6,
+                (wood + coal + uranium) / 100
+            )
+        elif input_identifier == 'ct':
+            # CityTiles
             citytile_pos = unit_id.split(' ')
 
             if citytile_pos[0] == strs[3] and citytile_pos[1] == strs[4]:  #　自分自身なら
                 x = int(strs[3]) + x_shift
                 y = int(strs[4]) + y_shift
-                b[20:22, x, y] = (
+                b[:2, x, y] = (
                     1,
                     cities[city_id]
                 )
@@ -180,7 +268,7 @@ def make_input(obs, unit_id):
     return b
 
 
-class LuxDataset(Dataset):
+class LuxDataset_worker(Dataset):
     def __init__(self, obses, samples):
         self.obses = obses
         self.samples = samples
@@ -191,7 +279,22 @@ class LuxDataset(Dataset):
     def __getitem__(self, idx):
         obs_id, unit_id, action = self.samples[idx]
         obs = self.obses[obs_id]
-        state = make_input(obs, unit_id)
+        state = make_input_worker(obs, unit_id)
+        
+        return state, action
+
+class LuxDataset_citytile(Dataset):
+    def __init__(self, obses, samples):
+        self.obses = obses
+        self.samples = samples
+        
+    def __len__(self):
+        return len(self.samples)
+
+    def __getitem__(self, idx):
+        obs_id, unit_id, action = self.samples[idx]
+        obs = self.obses[obs_id]
+        state = make_input_citytile(obs, unit_id)
         
         return state, action
 
@@ -212,14 +315,15 @@ class BasicConv2d(nn.Module):
         return h
 
 
-class LuxNet(nn.Module):
+class LuxNet_worker(nn.Module):
     def __init__(self):
         super().__init__()
         layers, filters = 12, 32
-        input_layers = 22
+        input_layers = 20
+        action_num = 5
         self.conv0 = BasicConv2d(input_layers, filters, (3, 3), True)
         self.blocks = nn.ModuleList([BasicConv2d(filters, filters, (3, 3), True) for _ in range(layers)])
-        self.head_p = nn.Linear(filters, 5, bias=False)
+        self.head_p = nn.Linear(filters, action_num, bias=False)
 
     def forward(self, x):
         h = F.relu_(self.conv0(x))
@@ -229,7 +333,25 @@ class LuxNet(nn.Module):
         p = self.head_p(h_head)
         return p
 
-def train_model(model, dataloaders_dict, criterion, optimizer, num_epochs):
+class LuxNet_citytile(nn.Module):
+    def __init__(self):
+        super().__init__()
+        layers, filters = 12, 32
+        input_layers = 20
+        action_num = 2 # buil_cart なし
+        self.conv0 = BasicConv2d(input_layers, filters, (3, 3), True)
+        self.blocks = nn.ModuleList([BasicConv2d(filters, filters, (3, 3), True) for _ in range(layers)])
+        self.head_p = nn.Linear(filters, action_num, bias=False)
+
+    def forward(self, x):
+        h = F.relu_(self.conv0(x))
+        for block in self.blocks:
+            h = F.relu_(h + block(h))
+        h_head = (h * x[:,:1]).view(h.size(0), h.size(1), -1).sum(-1)
+        p = self.head_p(h_head)
+        return p
+
+def train_model(model, dataloaders_dict, criterion, optimizer, num_epochs, model_type):
     best_acc = 0.0
 
     for epoch in range(num_epochs):
@@ -270,8 +392,9 @@ def train_model(model, dataloaders_dict, criterion, optimizer, num_epochs):
             print(f'Epoch {epoch + 1}/{num_epochs} | {phase:^5} | Loss: {epoch_loss:.4f} | Acc: {epoch_acc:.4f}')
         
         if epoch_acc > best_acc:
-            traced = torch.jit.trace(model.cpu(), torch.rand(1, 22, 32, 32)) # state_shape 何これ？
-            traced.save('model.pth')
+
+            traced = torch.jit.trace(model.cpu(), torch.rand(1, 20, 32, 32)) # state_shape 何これ？
+            traced.save(f'{model_type}_model.pth')
             best_acc = epoch_acc
 
 
@@ -283,25 +406,30 @@ if __name__ == "__main__":
     seed_everything(seed)
 
     episode_dir = '/home/ubuntu/work/codes/imitation_learning/input/episodes'
-    obses, samples = create_dataset_from_json(episode_dir)
-    print('obses:', len(obses), 'samples:', len(samples))
+    obses, woker_samples, citytile_samples = create_dataset_from_json(episode_dir)
+    print('obses:', len(obses), 'woker_samples:', len(woker_samples), 'citytile_samples:', len(citytile_samples))
 
-    labels = [sample[-1] for sample in samples]
+    woker_labels = [sample[-1] for sample in woker_samples]
     actions = ['north', 'south', 'west', 'east', 'bcity']
-    for value, count in zip(*np.unique(labels, return_counts=True)):
+    for value, count in zip(*np.unique(woker_labels, return_counts=True)):
         print(f'{actions[value]:^5}: {count:>3}')
 
-    model = LuxNet()
-    train, val = train_test_split(samples, test_size=0.1, random_state=42, stratify=labels)
+    citytile_labels = [sample[-1] for sample in citytile_samples]
+    actions = ['research', 'build_worker', 'build_cart']
+    for value, count in zip(*np.unique(citytile_labels, return_counts=True)):
+        print(f'{actions[value]:^5}: {count:>3}')
+
+    model = LuxNet_worker()
+    train, val = train_test_split(woker_samples, test_size=0.1, random_state=42, stratify=woker_labels)
     batch_size = 64
     train_loader = DataLoader(
-        LuxDataset(obses, train), 
+        LuxDataset_worker(obses, train), 
         batch_size=batch_size, 
         shuffle=True, 
         num_workers=2
     )
     val_loader = DataLoader(
-        LuxDataset(obses, val), 
+        LuxDataset_worker(obses, val), 
         batch_size=batch_size, 
         shuffle=False, 
         num_workers=2
@@ -310,10 +438,33 @@ if __name__ == "__main__":
     criterion = nn.CrossEntropyLoss()
     optimizer = torch.optim.AdamW(model.parameters(), lr=1e-3)
 
-    train_model(model, dataloaders_dict, criterion, optimizer, num_epochs=3)
+    train_model(model, dataloaders_dict, criterion, optimizer, num_epochs=1, model_type="woker")
 
-    # from kaggle_environments import make
+    model = LuxNet_citytile()
+    train, val = train_test_split(citytile_samples, test_size=0.1, random_state=42, stratify=citytile_labels)
+    batch_size = 64
+    train_loader = DataLoader(
+        LuxDataset_citytile(obses, train), 
+        batch_size=batch_size, 
+        shuffle=True, 
+        num_workers=2
+    )
+    val_loader = DataLoader(
+        LuxDataset_citytile(obses, val), 
+        batch_size=batch_size, 
+        shuffle=False, 
+        num_workers=2
+    )
+    dataloaders_dict = {"train": train_loader, "val": val_loader}
+    criterion = nn.CrossEntropyLoss()
+    optimizer = torch.optim.AdamW(model.parameters(), lr=1e-3)
+
+    train_model(model, dataloaders_dict, criterion, optimizer, num_epochs=1, model_type="citytile")
+
+    from kaggle_environments import make
     
     # env = make("lux_ai_2021", configuration={"width": 24, "height": 24, "loglevel": 2, "annotations": True}, debug=False)
     # steps = env.run(['agent.py', 'agent.py'])
     # env.render(mode="ipython", width=1200, height=800)
+
+    pass
